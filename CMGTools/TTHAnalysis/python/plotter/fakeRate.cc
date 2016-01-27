d3e1bee8e6a53edc12e5c5bc036891b819adbe3d
Giovanni <gpetruc@gmail.com>
2016-01-27 17:15:42 +0100

    Fake rate weight functions with only 8 arguments, to avoid crashes in root6

--- CMGTools/TTHAnalysis/python/plotter/fakeRate.cc
+++ CMGTools/TTHAnalysis/python/plotter/fakeRate.cc
@@ -191,6 +191,18 @@ float fakeRateWeight_2lssCB(float l1pt, float l1eta, int l1pdgId, float l1relIso
                             l2pt, l2eta, l2pdgId, l2relIso, WP, 0);
 }
 
+float fakeRateWeight_2lss(float l1pt, float l1eta, int l1pdgId, float l1pass,
+                            float l2pt, float l2eta, int l2pdgId, float l2pass) 
+{
+    return fakeRateWeight_2lssCB_i(l1pt, l1eta, l1pdgId, -l1pass,
+                            l2pt, l2eta, l2pdgId, -l2pass, -0.5, 0);
+}
+float fakeRateWeight_2lss_2(float l1pt, float l1eta, int l1pdgId, float l1pass,
+                            float l2pt, float l2eta, int l2pdgId, float l2pass) 
+{
+    return fakeRateWeight_2lssCB_i(l1pt, l1eta, l1pdgId, -l1pass,
+                            l2pt, l2eta, l2pdgId, -l2pass, -0.5, 2);
+}
 
 float fakeRateWeight_2lssSyst(float l1pt, float l1eta, int l1pdgId, float l1mva,
                          float l2pt, float l2eta, int l2pdgId, float l2mva, float WP, 
